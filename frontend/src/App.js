3bc7d20e03c5e79e22ff5efa6c1d384f2e58356f
florinagcotoc <florinacotoc@gmail.com>
2022-09-28 13:59:58 +0300

    Added Login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import SignUpScreen from './screens/Authentication/SignUpScreen';
+import LoginScreen from './screens/Authentication/LoginScreen';
 import './index.css'
 
 function App(){
@@ -7,6 +8,7 @@ function App(){
     <Router>
         <Routes>
           <Route path='/inregistrare' element={<SignUpScreen/>}></Route>
+          <Route path='/conectare' element={<LoginScreen/>}></Route>
         </Routes>
     </Router>
   );
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -2,6 +2,11 @@ import {
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
     USER_REGISTER_FAIL,
+    
+    USER_LOGIN_REQUEST,
+    USER_LOGIN_SUCCESS,
+    USER_LOGIN_FAIL,
+    USER_LOGOUT,
 
 } from '../constants/userConstants'
 import axios from 'axios'
@@ -40,3 +45,47 @@ export const register_func = (formRegisterData) => async (dispatch) => {
 
     }
 };
+
+
+export const login_func = (email, password) => async (dispatch) => {
+
+    try {
+        dispatch({
+            type: USER_LOGIN_REQUEST
+        })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+        const {data} = await axios.post(`${process.env.REACT_APP_BACKEND}/api/users/login/`,
+                        {"email":email, "password": password},
+                        config )
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+        
+    }
+    catch (error) {
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message,
+        })
+
+    }
+};
+
+
+export const logout_func = () => (dispatch) => {
+    localStorage.removeItem('userInfo')
+    dispatch ({
+        type:USER_LOGOUT
+    })
+}
