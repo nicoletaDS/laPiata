b413261bb18b6b59b8e02b30c1abdfbc7d3f2126
florinagcotoc <florinacotoc@gmail.com>
2022-09-28 13:30:05 +0300

    Created custom serializer for login functionality

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,6 @@
 from djoser.serializers import UserCreateSerializer
 from django.contrib.auth import  get_user_model
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 # retrieve the user model
 User = get_user_model()
@@ -8,4 +9,13 @@ User = get_user_model()
 class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = User
-        fields = ('id','first_name', 'last_name', 'name', 'email', 'phone', 'is_customer', 'image_profile')
+        fields = ('id','first_name', 'last_name', 'email', 'phone', 'is_customer', 'image_profile')
+
+
+class  MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        serializer = UserCreateSerializer(self.user).data
+        for key, value in serializer.items():
+            data[key] = value
+        return data
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from users import views
+from users.views import MyTokenObtainPairView
 
-urlpatterns=[]
+urlpatterns=[
+    path('users/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+]
